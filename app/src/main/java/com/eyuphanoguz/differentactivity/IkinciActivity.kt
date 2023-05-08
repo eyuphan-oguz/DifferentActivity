1a7d575e98a24d3cd27f52d04534ac98cb8c81a5
eyuphan-oguz <eyuphanoguz@yahoo.com>
2023-05-08 18:53:43 +0300

    send data to different page

--- app/src/main/java/com/eyuphanoguz/differentactivity/IkinciActivity.kt
+++ app/src/main/java/com/eyuphanoguz/differentactivity/IkinciActivity.kt
@@ -2,10 +2,20 @@ package com.eyuphanoguz.differentactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class IkinciActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ikinci)
+
+        val intent = intent
+        var alinanVeri = intent.getStringExtra("yollananVeri")
+        var textDegeri = findViewById<TextView>(R.id.textView2)
+        Log.e("gelendata","${alinanVeri}")
+        textDegeri.text=alinanVeri.toString()
     }
+
+
 }
--- app/src/main/java/com/eyuphanoguz/differentactivity/MainActivity.kt
+++ app/src/main/java/com/eyuphanoguz/differentactivity/MainActivity.kt
@@ -4,15 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
     fun changeActivity(view: View){
+        var kullaniciVerisi = findViewById<EditText>(R.id.editText).text.toString()
+
         val intent = Intent(applicationContext, IkinciActivity::class.java)
+        intent.putExtra("yollananVeri",kullaniciVerisi)
         startActivity(intent)
     }
 }
