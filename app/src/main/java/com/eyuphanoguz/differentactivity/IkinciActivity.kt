05c5bcde0af89fec49a4e368e73bda92624cdde0
eyuphan-oguz <eyuphanoguz@yahoo.com>
2023-05-08 19:54:21 +0300

    Add to alert message

--- app/src/main/java/com/eyuphanoguz/differentactivity/IkinciActivity.kt
+++ app/src/main/java/com/eyuphanoguz/differentactivity/IkinciActivity.kt
@@ -1,11 +1,14 @@
 package com.eyuphanoguz.differentactivity
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class IkinciActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +25,22 @@ class IkinciActivity : AppCompatActivity() {
     fun backPush(view: View){
         val intent = Intent(applicationContext, MainActivity::class.java)
         startActivity(intent)
+    }
+
+    fun showAlertMessage(view: View){
+        val uyariMesaji = AlertDialog.Builder(this)
+        uyariMesaji.setTitle("Sifre Hatasi")
+        uyariMesaji.setMessage("Lutfen sifreyi giriniz")
+        uyariMesaji.setPositiveButton("Evet",DialogInterface.OnClickListener({dialogInterface, i ->
+            Toast.makeText(this,"Evete tiklandi",Toast.LENGTH_LONG).show()
+
+        }))
+        uyariMesaji.setNegativeButton("Hayir",({dialogInterface, i ->
+            Toast.makeText(this,"Hayira tiklandi",Toast.LENGTH_LONG).show()
+        }))
+
+        uyariMesaji.show()
+
     }
 
 
