79ef3d6502014a912ef13ccbea8f77a683302e3b
eyuphan-oguz <eyuphanoguz@yahoo.com>
2023-05-08 18:16:26 +0300

    change page activity

---
+++ app/src/main/java/com/eyuphanoguz/differentactivity/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.eyuphanoguz.differentactivity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun changeActivity(view: View){
+        val intent = Intent(applicationContext, IkinciActivity::class.java)
+        startActivity(intent)
+    }
+}
